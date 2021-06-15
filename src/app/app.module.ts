9ef732370d277b61f480769d8eb84b5d9429654d
zaleck-mov <alexisgunitt@gmail.com>
2021-06-15 03:05:36 -0500

    boton offcanvas y componente list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedComponent } from './components/shared/shared.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SharedComponent
+    SharedComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
